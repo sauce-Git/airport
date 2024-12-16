02b85fb684f9b5855d4b3af1a0b5fc79986dd6a7
sauce-Git <dadad1004@naver.com>
2024-12-17 04:07:58 +0900

    fix: fix redis config

--- src/main/java/com/travel/airport/config/SessionConfig.java
+++ src/main/java/com/travel/airport/config/SessionConfig.java
@@ -9,12 +9,13 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisIndexedWebSession;
+import org.springframework.session.data.redis.config.ConfigureReactiveRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 import org.springframework.web.server.session.CookieWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
 
 @Configuration
-@EnableRedisIndexedWebSession
+@EnableRedisWebSession
 public class SessionConfig {
 
   @Value("${spring.data.redis.host}")
@@ -26,6 +27,11 @@ public class SessionConfig {
   @Bean
   LettuceConnectionFactory connectionFactory() {
     return new LettuceConnectionFactory(redisHost, redisPort);
+  }
+
+  @Bean
+  ConfigureReactiveRedisAction configureReactiveRedisAction() {
+    return ConfigureReactiveRedisAction.NO_OP;
   }
 
   @Bean
